a2dc4e87ef8dc9f4a76925643bcc07859f8ca2a1
Fziliotti <ffz.ziliotti@gmail.com>
2018-12-05 16:30:34 -0200

    alterando estrutura do projeto

---
+++ BackEnd/graphApi.js
@@ -0,0 +1,103 @@
+
+const Graph = require('./Graph')
+const fs = require('fs')
+
+// UMA PROMISE NUNCA DISPARA THROWS, PARA ISSO EXISTE O REJECT
+function parseFile(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, "utf8", (error, data) => {
+            if (error) return reject(new Error("Caminho inválido"))
+            
+            let array = data.split("\n")
+                .map(l => l.replace("\r", "").split("\t").map(Number))
+            resolve(array)
+        })
+    })
+}
+
+function arrayInfo(data) {
+    let vertexes = new Set()
+    data.map(edge => {
+        vertexes.add(edge[0])
+        vertexes.add(edge[1])
+    })    
+
+    return {
+        vertexes: Array.from(vertexes),
+        biggest: Array.from(vertexes).reduce((a, b) => Math.max(a, b)),
+        lowest: Array.from(vertexes).reduce((a, b) => Math.min(a, b))
+    }
+}
+
+// para nao ter que reemitir o erro, foi colocado a sintaxe async e await
+async function parseGraph(path) {
+    let data = await parseFile(path)
+    let graph = new Graph(arrayInfo(data).vertexes)
+    data.map(edge => graph.insertEdge(edge[0], edge[1]))
+    return graph
+}
+
+function compareFileGraph(path, graph) {
+    return new Promise(resolve => {
+        parseFile(path).then(array => {
+            let validation = true
+            array.map(edge => validation *= graph.existEdge(edge[0], edge[1]))
+            resolve(new Boolean(validation).valueOf())
+        })
+    })
+}
+
+function bfs(graph, rootVertex, visited){
+    let queue = []
+    visited.push(rootVertex) //marque a raiz
+    queue.push(rootVertex)  //insira a raiz na fila
+    while(queue.length){
+        let firstVertex = queue[0] //tira o primeiro elemento e o retorna
+        // console.log("firstVertex: " + firstVertex)
+        // console.log( graph.edges.get(firstVertex))                
+        graph.edges.get(firstVertex).forEach(vertex => {
+            if(!visited.includes(vertex)){
+                visited.push([firstVertex, vertex])
+                visited.push(vertex) //marque o vertice
+                queue.push(vertex)  //insire o vertice
+            } else if (queue.includes(vertex)){ 
+                visited.push([firstVertex, vertex])
+            }
+        })
+        queue.shift()      
+    }
+}
+
+function numCompConexas(graph){
+    console.log("Rodando função numCompConexas")
+    let rootQueue = new Set()
+    graph.vertexes.forEach(elem => rootQueue.add(elem))
+
+    let quantity = 1
+    let rootVisited = [[]]  
+    rootQueue.forEach(vertex =>{
+        let visited = []
+        bfs(graph, vertex, visited)
+        rootVisited.push(visited)
+    })
+
+    // console.log(rootVisited)
+    rootVisited.forEach(bfsVisited =>{
+       let copyBfsVisited = bfsVisited
+       rootVisited.forEach(anotherBfsVisited => {
+            copyBfsVisited.filter(value => -1 !== anotherBfsVisited.indexOf(value))
+            if(copyBfsVisited.lenght == 0) quantity++
+            copyBfsVisited = bfsVisited
+        })
+    })
+    return quantity
+} 
+
+
+module.exports = {
+    parseFile,
+    parseGraph,
+    compareFileGraph,
+    bfs,
+    numCompConexas
+}
---
+++ BackEnd/server.js
@@ -0,0 +1,73 @@
+// Importação de libs e arquivos externos
+const http = require("http");
+const graphApi = require("./graphApi");
+const PORTA = 80;
+// HEADER da Requisição
+const HEADERS = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*"
+};
+
+// configuração do servidor que será levantado na porta 80
+const app = http.createServer(onConnection);
+app.listen(PORTA);
+app.on("listening", () => console.log("Escutando na porta " + PORTA));
+
+// callback  que vai ler o grafo de maneira assincrona e retorna o grafo 
+function onConnection(req, res) {
+    leGrafo(req, res)
+        .catch(e => {
+            res.writeHead(500, HEADERS);
+            res.end(JSON.stringify({
+                success: false,
+                error: e.message
+            }));
+        });
+}
+
+async function leGrafo(req, res) {
+    let path = req.url; //pega url da requisicao
+    path = path.substring(1); // tira a Barra '/'
+    path = decodeURIComponent(path); //decodifica a URI, tirando os caracteres especiais
+    path = "../" + path; //voltar uma pasta 
+    try {
+        let graph = await graphApi.parseGraph(path); // grafo espera o retorno do parseGraph
+        let objGrafo = {...graph};
+        objGrafo.vertexes = [...objGrafo.vertexes]; // transforma o Set em Array
+        
+        // gambiarra abaixo
+        objGrafo._edges = objGrafo.edges;
+        objGrafo.edges = new Object();
+        [...objGrafo._edges.keys()].forEach(k => objGrafo.edges[k] = [...objGrafo._edges.get(k)]);
+        delete objGrafo._edges;
+
+        // ATRIBUTOS DO OBJETO RETORNADO COMO RESPOSTA
+        objGrafo.arquivo = await path.substring(6);
+        objGrafo.totalVertexes = await graph.totalVertexes();
+        objGrafo.vertexDegree = graph.vertexDegree(2);
+
+        objGrafo.vertexDegrees =  graph.printVerticesDegrees();
+
+        objGrafo.mediumDegree = await graph.mediumDegree();
+        objGrafo.graphDensity = await graph.graphDensity();
+        objGrafo.avGroupCoef = await graph.averageGroupingCoefficient();
+        // objGrafo.neighbours = graph.qtdNeighbours(5);
+        objGrafo.printAdjList = await graph.printAdjList();
+        // objGrafo.numCompConexas = await graphApi.numCompConexas(graph);
+       
+
+        //OBJETO RESPOSTA DA REQUISICAO
+        objGrafo = {
+            success: true,
+            grafo: objGrafo
+        };
+
+        objGrafo = JSON.stringify(objGrafo);
+        res.writeHead(200, HEADERS);
+        res.end(objGrafo); // a respota contem um objeto com os campos success e o grafo construido
+
+    } catch (err) {
+        console.log("Erro no servidor: " + err);
+    }
+
+}
